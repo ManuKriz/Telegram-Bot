41085a9cd93dbe203605bec7eb11b61cc1a70320
Manu <manukrishnaos45@gmail.com>
2022-07-15 19:27:38 +0530

    Update MksBot.py

--- plugin/MksBot.py
+++ plugin/MksBot.py
@@ -2,4 +2,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.private)
 async def onMsg(bot, msg):
-  await msg.reply(f"Hey {msg.from_user.first_name} I'm Alive")
+  await msg.reply("[Oops](https://t.me/MovieS_HooD/139)")
